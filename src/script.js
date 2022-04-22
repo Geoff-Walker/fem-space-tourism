dae15113bec53ab78c6b22e61ab7d00e953d8e75
Geoff-Walker <geoffwalker1980@gmail.com>
2022-04-22 15:41:12 +0100

    mobile menuu complete

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
 const menu = document.querySelector('.header-menu');
+const nav = document.querySelector('.header-nav');
 const menuClose = document.querySelector('.header-close');
 const menuList = document.querySelector('.header-nav-list');
 
-menu.addEventListener('click', openMobMenu());
-menuClose.addEventListener('click', openMobMenu());
+menu.addEventListener('click', openMobMenu);
+menuClose.addEventListener('click', openMobMenu);
 
 function openMobMenu() {
+	nav.classList.toggle('header-nav-mobile-hide');
 	menu.classList.toggle('mob-hidden');
 	menuClose.classList.toggle('mob-hidden');
 	menuList.classList.toggle('mob-hidden');
