3b1a354ba454ca8cc1ae14a204d8bc422feadaee
Geoff-Walker <geoffwalker1980@gmail.com>
2022-05-10 14:53:53 +0100

    Mobile - Home & Destination
    In mobile view home and Destination have been completed.

--- src/script.js
+++ src/script.js
@@ -2,9 +2,11 @@ const menu = document.querySelector('.header-menu');
 const nav = document.querySelector('.header-nav');
 const menuClose = document.querySelector('.header-close');
 const menuList = document.querySelector('.header-nav-list');
+const mobExplore = document.querySelector('.home-container-explore-mobile');
 
 menu.addEventListener('click', openMobMenu);
 menuClose.addEventListener('click', openMobMenu);
+mobExplore.addEventListener('click', openMobMenu);
 
 function openMobMenu() {
 	nav.classList.toggle('header-nav-mobile-hide');
